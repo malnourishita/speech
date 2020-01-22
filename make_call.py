fed4c5d45100fa9712aa72a2cced9c8e6221463e
malnourishita <56637975+malnourishita@users.noreply.github.com>
2020-01-22 22:35:21 +0800

    Update make_call.py

--- make_call.py
+++ make_call.py
@@ -1,15 +1,16 @@
 
 from twilio.rest import Client
 
-account_sid = 'AC1515c031e3333369b204c6d7de071c70'
-auth_token = '7eb60f36cf8598daa668a4da800063e4'
+account_sid = 'ACcf1318a402be1d709edf69783888f986'
+auth_token = '7d65cd5377b5c50a1ee9da8aaa2e94af'
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
                         to="+639437057481",
-                        from_="+12019493856",
+                        from_="+16504194114",
                         url="http://demo.twilio.com/docs/voice.xml"
                     )
 
 print(call.sid)
  
+
